4fb03aeb29e6898e859d9a3c58520ff314a7b129
seanvoss <seanvoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-25 10:41:34 +0000

    Adding Templates
    git-svn-id: https://plugins.svn.wordpress.org/shatner-name-your-own-price-for-woocommerce/trunk@793512 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- name_price_wc_plugin.php
+++ name_price_wc_plugin.php
@@ -4,12 +4,28 @@ class SV_WC_Donation extends WC_Cart
 
     public function __construct()
     {
-        add_filter('woocommerce_get_price_html',           array($this,  'add_price_html'));
+    //    add_filter('woocommerce_get_price_html',           array($this,  'add_price_html'));
+        add_filter('woocommerce_locate_template',       array($this, 'template_override'),10,3);
         add_action('woocommerce_product_options_pricing',  array($this,  'add_donation_checkbox'));
         add_action('save_post',                            array($this,  'set_named_price'));
         add_action('woocommerce_add_to_cart',              array($this,  'add_to_cart_hook'));
         add_action('woocommerce_before_calculate_totals',  array($this,  'add_custom_price' ));
         add_action('init', array($this, 'init_css'));
+    }
+
+    public function template_override($template, $template_name, $template_path ) {
+    // Modification: Get the template from this plugin, if it exists
+
+    $plugin_path = untrailingslashit( plugin_dir_path( __FILE__ ) ). '/templates/';
+    
+    if ( file_exists( $plugin_path . $template_name ) )
+    {
+      $template = $plugin_path . $template_name;
+      return $template;
+    }
+
+    return $template;
+
     }
 
     public function init_css()
@@ -51,28 +67,32 @@ class SV_WC_Donation extends WC_Cart
 
     public function add_donation_checkbox($content)
     {
-       global $post ;
+       global $post;
        woocommerce_wp_checkbox(array('id' => '_own_price', 'class' => 'wc_own_price short', 'label' => __( 'Name you own price', 'woocommerce' ), 'value'=> 'yes', 'cbvalue' => get_post_meta($post->ID, '_own_price', true ) ? 'yes' : 'no'));
     }
 
     public function add_price_html($content)
     {
         global $post;
+        var_dump($content);
+        if(!$post)
+            return;
         $post = get_post_meta($post->ID, '_own_price', true);
-        print "
-        <script>
-            jQuery(function($){
-                $('.cart').submit(function(){
-                    $('#price').clone().attr('type','hidden').appendTo($('form.cart'));
-                    return;
-                });
-            });
-        </script>";
+        
         if($post == 'yes')
         {
+            print "
+            <script>
+                jQuery(function($){
+                    $('.cart').submit(function(){
+                        $('#price').clone().attr('type','hidden').appendTo($('form.cart'));
+                        return;
+                    });
+                });
+            </script>";
             return sprintf("<div class='name_price'><label>%s</label><input name='price' id='price' type='text' /></div>", "Name your own price");
         }
-        return $content;
+        return ;
         
     }
 
@@ -83,7 +103,6 @@ class SV_WC_Donation extends WC_Cart
                 $named_price = $woocommerce->session->__get($key .'_named_price');
                 if($named_price)
                 {
-                    error_log(var_export($named_price,1));
                     $value['data']->price = $named_price;
                 }
         }
