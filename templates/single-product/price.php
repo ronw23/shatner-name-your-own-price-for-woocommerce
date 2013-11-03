e9c22bf88193abf4549d02829f13c05821dd50fd
seanvoss <seanvoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-03 15:41:15 +0000

    Template Update
    git-svn-id: https://plugins.svn.wordpress.org/shatner-name-your-own-price-for-woocommerce/trunk@798104 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/single-product/price.php
+++ templates/single-product/price.php
@@ -12,20 +12,33 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 global $post, $product;
 
 
- $own_price = get_post_meta($post->ID, '_own_price', true);
+$own_price = get_post_meta($post->ID, '_own_price', true);
         
-        if($own_price == 'yes')
-        {
+if($own_price == 'yes')
+{
 ?>
-            <script>
-                jQuery(function($){
-                    $('.cart').submit(function(){
-                        $('#price').clone().attr('type','hidden').appendTo($('form.cart'));
-                        return;
-                    });
-                });
-            </script>
-            <div class='name_price'><label>Name your own price</label><input name='price' id='price' type='text' /></div><?
+    <script>
+        jQuery(function($){
+            $('.cart').submit(function(){
+                $('#price').clone().attr('type','hidden').appendTo($('form.cart'));
+                return;
+            });
+        });
+    </script>
+    <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+        <div class="name_price">
+            <p itemprop="name_price price" class="price">
+              <label>Name your own price</label>
+              <input name='price' id='price' type='text' />
+            </p>
+        </div>
+
+        <meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
+        <link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+
+    </div>
+
+    <?
         } else {
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
