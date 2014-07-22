c0d6da1c3cc03a6cda5c7e686ebcd7470b038f75
seanvoss <seanvoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-22 15:49:25 +0000

    Cutting 1.7
    git-svn-id: https://plugins.svn.wordpress.org/shatner-name-your-own-price-for-woocommerce/trunk@952893 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shatner.js
+++ shatner.js
@@ -1,21 +1,40 @@
 jQuery(function($){
-        $('.cart').click(function(){
-            if($('.name_price').length && !parseFloat($('input.name_price').val()))
-            {
-                alert('Enter a price to continue');
-                return false;
-            }  
+  $('.cart').click(function(){
 
-            if($('#minimum_price').length && parseFloat($('#minimum_price').val()) >= parseFloat($('input.name_price').val()))
-            {
-                alert('Price must be higher than the minimum price');
-                return false;
-            } 
+// Test to see if the price exceeds the minimum price (ie the Suggested Price):
 
-        });
-        $('.cart').submit(function(){
-          
-            $('<input name="price" />').val($('input.name_price').val()).attr('type','hidden').appendTo($('form.cart'));
-            return;
-        });
+    if($('#minimum_price').length && parseFloat($('#minimum_price').val()) >= parseFloat($('input.name_price').val()))
+    {
+        alert('Please offer a price higher than the Minimum Price.');
+        return false;
+    } 
+// See if the price input is zero (because we want people to be able to choose a free option: NEEDS LOGIC FOR SET MINIMUM
+
+    else if(parseFloat($('input.name_price').val()) == 0)           
+    {
+        return;
+    }  
+
+// Test to see if the input field has been left blank:
+
+    else if($('.name_price').length && !parseFloat($('input.name_price').val()))
+    {
+        alert('Please enter a price in the Price field!');
+        return false;
+    }
+// Test to see if input field is non-negative:
+
+    else if(parseFloat($('input.name_price').val()) < 0)
+    {
+        alert("Look here, old chap, that's just not on. Please enter a positive number, or zero. Tsk tsk.");
+        return false;
+    }
+
+  });
+  $('.cart').submit(function(){
+  
+    $('<input name="price" />').val($('input.name_price').val()).attr('type','hidden').appendTo($('form.cart'));
+    return;
+  });
 });
+
